ab92dd144d644e61b602e5a4a4f6264ffa76b2ee
Clément Chanchevrier <clement.chanchevrier@gmail.com>
2023-02-02 07:38:47 +0100

    build(vite): 📌 use babel instead of swc

--- packages/app/web/vite.config.ts
+++ packages/app/web/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import eslint from 'vite-plugin-eslint';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
