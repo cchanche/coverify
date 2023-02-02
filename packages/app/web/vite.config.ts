69110b8d1776a6bc53fb2152dbbec7c093c8edc7
Clément Chanchevrier <clement.chanchevrier@gmail.com>
2023-02-02 06:57:40 +0100

    feat(docker): 🔧 use docker-compose

---
+++ packages/app/web/vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig, loadEnv } from 'vite';
+import eslint from 'vite-plugin-eslint';
+import react from '@vitejs/plugin-react-swc';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, '.');
+  return {
+    plugins: [eslint(), react()],
+    server: {
+      host: true,
+      port: Number(env.VITE_SERVER_PORT) || 3000,
+    },
+  };
+});
--- packages/web/app/web/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import eslint from 'vite-plugin-eslint'
-import react from '@vitejs/plugin-react-swc'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [eslint(), react()],
-})
